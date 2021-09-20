5d742c0da503bd3d7af21a0bc01ce858a58912d9
janvi809 <67974104+janvi809@users.noreply.github.com>
2021-09-20 20:10:31 +0530

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,6 +3,11 @@ const Discord = require('discord.js');
 const MusicBot = require('music.bot');
 const { Client } = require('discord.js');
 const client = new Client();
+let puppeteer = require("puppeteer");
+let url = "https://www.mygov.in/covid-19";
+let request = require("request");
+let cheerio = require("cheerio");
+let fs  = require("fs");
 
 
 const { Player } = require('discord-player');
@@ -12,8 +17,6 @@ client.player = player;
 const settings = new MusicBot({
     botPrefix: '#',//The prefix for your bot that you will use with commands
     botClient: client,//Your client name in discord js
- // ytApiKey: `You Api yt`, //If you want to use a YouTube key from your own
- // hideHelp: true, //If you want to hide the help order
 });
 
 client.on('message', message => {
@@ -35,7 +38,66 @@ console.log(`[${message.author.tag}] : ${message.content}`);
 if(message.content === 'hello' || message.content==='Hello'){
     message.channel.send('Hola amigo!! wssup:)');
 } 
+
+if(message.content=='#covid'){
+    let browserStartPromise = puppeteer.launch({
+        // visible 
+        headless: false,
+        // type 1sec // slowMo: 1000,
+        defaultViewport: null,
+        // browser setting 
+        args: ["--start-maximized", "--disable-notifications"]
+    });
+    let page, browser, rTab;
+    browserStartPromise
+        .then(function (browserObj) {
+            console.log("Browser opened");
+            // new tab 
+            browser = browserObj
+            let browserTabOpenPromise = browserObj.newPage();
+            return browserTabOpenPromise;
+        }).then(function (newTab) {
+            page = newTab
+            console.log("new tab opened ")
+            let gPageOpenPromise = newTab.goto("https://www.mygov.in/covid-19", {waitUntil : 'load'});
+            return gPageOpenPromise;
+        })
+         
+       
+}
+
+if(message.content=='#covidinfo'){
+    request(url, cb);
+    function cb(error, response, html) {
+        if (error) {
+            console.log(error); // Print the error if one occurred
+        } else if (response.statusCode == 404) {
+            console.log("Page Not Found")
+        }
+        else {
+            // console.log(html); // Print the HTML for the request made 
+            dataExtracter(html);
+        }
+    }
+
+    function dataExtracter(html) {
+        let searchTool = cheerio.load(html);
+        fs.writeFileSync("covid.html",html);
+        let caseArr = searchTool(".icount");
+         console.log(caseArr.length);
+         let cases = searchTool(caseArr[0]).text();
+         let cases1 = searchTool(caseArr[1]).text();
+             message.channel.send("Active cases: " + cases);
+             message.channel.send("Total cases: " + cases1);
+        
+    }
+}
+
+
 });
+
+
+
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
